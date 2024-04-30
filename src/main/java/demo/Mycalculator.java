461d99c927b58816a0fd895f5b6830a2cb523fb9
Hima Bindu Golla <himabindug2424@gmail.com>
2024-04-30 08:40:03 +0530

    First maven project

---
+++ src/main/java/demo/Mycalculator.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Mycalculator {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		Mycalculator calc = new Mycalculator();
+	    System.out.println("sum is"+calc.sum(20,10));
+		// TODO Auto-generated method stub
+
+	}
+
+}
